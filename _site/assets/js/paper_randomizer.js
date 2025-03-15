84dfd3d132f42dacb0f3f20587c20e4e63d03f72
Luke Currier <currier.l@northeastern.edu>
2025-03-15 12:09:21 -0400

    removing unecessary files

--- _site/assets/js/paper_randomizer.js
+++ _site/assets/js/paper_randomizer.js
@@ -1,23 +1,21 @@
-// List of PDF files
+// list of PDF files
 const pdfFiles = [
-    'assets/papers/document1.pdf',
-    'assets/papers/document2.pdf'
-    // Add more PDFs here
+    //'assets/papers/document1.pdf',
 ];
 
-// Function to get a random PDF from the list
+// function to get a random PDF from the list
 function getRandomPdf() {
     const randomIndex = Math.floor(Math.random() * pdfFiles.length);
     return pdfFiles[randomIndex];
 }
 
-// Function to display the PDF
+// display the PDF
 function displayPdf(pdfPath) {
     const pdfContainer = document.getElementById('pdf-container');
     pdfContainer.innerHTML = `<embed src="${pdfPath}" width="600" height="500" type="application/pdf">`;
 }
 
-// Add event listener to the button
+// add an event listener to the button
 document.getElementById('random-pdf-button').addEventListener('click', () => {
     const randomPdf = getRandomPdf();
     displayPdf(randomPdf);
